1ae3adb503322545e462e188aae79d26c46049ab
amanbhagat77 <37319472+amanbhagat77@users.noreply.github.com>
2020-03-17 16:46:18 +0530

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -1,6 +1,13 @@
-
 library(dplyr)
 library(ggplot2)
+library(scales)
+library(data.table)
+
+#LOADING THE DATASET
+pm25 <-readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code")
+
+#CODE TO PRODUCE REQUIRED PLOT
 
 mergedata <- merge(scc , baltimore,by = "SCC")
 motor.baltimore <- mergedata %>% group_by(year) %>% summarize(Emissions = sum(Emissions))
@@ -11,3 +18,6 @@ xlab("Year")+
 ylab(expression("Total tons of PM"[2.5]*"Emission"))+
 ggtitle(expression("Total tons of PM"[2.5]*"Emission by motor vehicles in Baltimore City"))+
 guides(fill = FALSE)
+
+dev.copy(png, file= "Plot5.png",height = 680 , width = 480) # Saving to png file
+dev.off()
