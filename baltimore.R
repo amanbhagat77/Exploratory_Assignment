72b51187ee103afc32bd9d37655e1f31e39d16d4
amanbhagat77 <37319472+amanbhagat77@users.noreply.github.com>
2020-03-17 15:18:33 +0530

    Delete baltimore.R

--- baltimore.R
+++
@@ -1,6 +0,0 @@
-baltimore <- subset(pm25,pm25 == "24510")
-baltimore_emissions <- tapply(baltimore$Emissions,baltimore$year,mean)
-baltimore_emissions <- as.data.frame(baltimore_emissions)
-setDT(baltimore_emissions,keep.rownames = "year")
-colnames(baltimore_emissions) <- c("year" , "Emissions")
-plot(baltimore_emissions$year,baltimore_emissions$Emissions,type = 'l',ylab = "Emissions(mean)",xlab = "Years",main = "Baltimore Emission Trend over the years"
